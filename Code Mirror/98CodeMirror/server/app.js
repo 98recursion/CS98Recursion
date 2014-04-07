f21ca359aa1b9ffc7642b0eb8b349106af63bb98
Eliza Hanson <eliza@ubuntu.(none)>
2014-04-07 17:12:49 -0400

    adding stuff to create a node server

---
+++ Code Mirror/98CodeMirror/server/app.js
@@ -0,0 +1,28 @@
+
+
+
+
+var express = require('express');
+var app = express();
+var routes = require('./routes');
+
+var hbs = require('hbs');
+
+var blogEngine = require('./blog');
+
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
+app.use(express.bodyParser());
+
+//This is for the style sheet which I dont need right now...
+//app.use(express.static('public'));
+
+app.get('/', routes.index);
+
+app.get('/about', routes.about);
+
+app.get('/article/:id', routes.articles);
+
+app.get('/debugger', routes.debug);
+
+app.listen(3000);
