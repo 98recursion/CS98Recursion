56e3401af955f8df424ce872fcd79af277f82127
Eliza Hanson <eliza@ubuntu.(none)>
2014-04-14 20:04:57 -0400

    WE CAN NOW USE CHROME BECAUSE WE HAVE A WORKING SERVER! SUPER COOL

--- Code Mirror/98CodeMirror/server/app.js
+++ Code Mirror/98CodeMirror/server/app.js
@@ -8,11 +8,23 @@ var routes = require('./routes');
 
 var hbs = require('hbs');
 
+//var ejs = require( 'ejs')
+
 var blogEngine = require('./blog');
 
 app.set('view engine', 'html');
 app.engine('html', hbs.__express);
 app.use(express.bodyParser());
+
+
+
+
+//Get rid of layout
+app.set('view options', { layout: false });
+
+//app.use(express.static(__dirname + '../TowersofHanoi'));
+app.use(express.static(__dirname ));
+
 
 //This is for the style sheet which I dont need right now...
 //app.use(express.static('public'));
@@ -21,10 +33,16 @@ app.get('/', routes.index);
 
 app.get('/towers', routes.towers);
 
+app.get('/queens', routes.queens);
+
+app.get('/code', routes.codeM);
+
+app.get('/theme', routes.theme);
+
+
+//app.get('../../TowersofHanoi/doc/docs.css', routes.test);
+
 app.get('/about', routes.about);
-
 app.get('/article/:id', routes.articles);
-
-app.get('/debugger', routes.debug);
 
 app.listen(3000);
